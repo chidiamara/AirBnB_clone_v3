9cff35f7c7169e02e1c871a6ee0d0fb2aee43275
Chidi <chidiekejiuba@gmail.com>
2023-05-30 05:26:25 +0100

    fixed space errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,10 +15,12 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """"Returns the number of each objects by type"""
-    objects =  {"amenities": storage.count("Amenity"),
-                "cities": storage.count("City"),
-                "places": storage.count("Place"),
-                "reviews": storage.count("Review"),
-                "states": storage.count("State"),
-                "users": storage.count("User")}
+    objects = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        }
     return jsonify(objects)
