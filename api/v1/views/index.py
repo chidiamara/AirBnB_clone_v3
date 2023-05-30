ac3ea4aa464740363b71fe940ced2e2a14622ecd
Chidi <chidiekejiuba@gmail.com>
2023-05-30 02:02:51 +0100

    fix conflict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,14 +10,3 @@ from models import storage
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """"Returns the number of each objects by type"""
-    objects = {"amenities": storage.count(amenities),
-              "cities": storage.count(cities),
-              "places": storage.count(places),
-              "reviews": storage.count(reviews),
-              "states": storage.count(states),
-              "users": storage.count(users)}
-    return jsonify(objects)
